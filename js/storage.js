75e17b1ab8c814d3ddcdbd48ad9dfb81be14ae04
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-11-21 19:40:33 +0900

    Create storage.js

---
+++ js/storage.js
@@ -0,0 +1,28 @@
+export async function saveData(data, password) {
+  const enc = new TextEncoder();
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    enc.encode(password),
+    "PBKDF2",
+    false,
+    ["deriveKey"]
+  );
+
+  const salt = crypto.getRandomValues(new Uint8Array(16));
+  const key = await crypto.subtle.deriveKey(
+    { name: "PBKDF2", salt, iterations: 100000, hash: "SHA-256" },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt"]
+  );
+
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const encrypted = await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv },
+    key,
+    enc.encode(JSON.stringify(data))
+  );
+
+  return { salt, iv, encrypted };
+}
